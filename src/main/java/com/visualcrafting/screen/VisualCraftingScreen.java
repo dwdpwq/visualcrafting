c6a8a7733166701788ac1205358e6cec23519945
dwdpwq <118658292+dwdpwq@users.noreply.github.com>
2026-09-24 16:40:40 +0800

    Keep wrapping button behavior while extracting dropdown

--- src/main/java/com/visualcrafting/screen/VisualCraftingScreen.java
+++ src/main/java/com/visualcrafting/screen/VisualCraftingScreen.java
@@ -5759,4 +5759,33 @@ public class VisualCraftingScreen extends AbstractContainerScreen<VisualCrafting
             this.mode5DurationEdit.setValue("30");
         }
     }
+
+    private static class WrappableButton
+        extends Button {
+        public WrappableButton(int x, int y, int width, int height, Component message, Button.OnPress onPress) {
+        super(x, y, width, height, message, onPress, DEFAULT_NARRATION);
+        }
+
+        @Override
+        public void renderString(GuiGraphics graphics, Font font, int color) {
+        Component message = this.getMessage();
+        int maxTextWidth = this.getWidth() - 6;
+        if (font.width(message) <= maxTextWidth) {
+        graphics.drawCenteredString(font, message, this.getX() + this.getWidth() / 2,
+                this.getY() + (this.getHeight() - 8) / 2, color);
+        } else {
+        List<FormattedCharSequence> lines = font.split(message, maxTextWidth);
+        int lineHeight = 9;
+        int totalTextHeight = lines.size() * lineHeight;
+        int startY = this.getY() + (this.getHeight() - totalTextHeight) / 2;
+        for (int i = 0; i < lines.size(); ++i) {
+        graphics.drawCenteredString(font, lines.get(i), this.getX() + this.getWidth() / 2,
+                startY + i * lineHeight, color);
+        }
+
+        }
+
+        }
+
+        }
 }
